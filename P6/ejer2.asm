81d288100c5636c266d1bd2d4a573ce832e3783b
Fernando Maceda <fernando.patricio.maceda@gmail.com>
2022-11-06 16:09:40 -0600

    Update ejer2.asm

--- P6/ejer2.asm
+++ P6/ejer2.asm
@@ -1,63 +1,61 @@
-;Pr醕tica 3 - Ejercicio2
-
-processor p16f877 				;Versi髇 del procesador a utilizar
+processor p16f877 			;Versi贸n del procesador a utilizar
 include <p16f877.inc> 			;Se cargan las librerias y el mapa de memoria
 
-	J EQU 0X20 					;Localidades para la subrutina de retraso 
-	K EQU 0X21 					;Se carga al vector RESET
+	J EQU 0X20 			;Localidades para la subrutina de retraso 
+	K EQU 0X21 			;Se carga al vector RESET
 	
-	goto INICIO 				;Su ubicaci髇 es en el espacio de memoria "inicio"
-	org 5h 						;Se ubica en el espacio de memoria 5 
+	goto INICIO 			;Su ubicaci贸n es en el espacio de memoria "inicio"
+	org 5h 				;Se ubica en el espacio de memoria 5 
 
 INICIO:
-	CLRF PORTA 					;limpiamos PORTA
-	BSF STATUS, RP0 			;Nos posicionamos en el banco 1 para poder trabajar con TRIS
-	BCF STATUS, RP1 			;Nos cambiamos al banco 1
-	MOVLW 00H 					;Se configura al puerto a y e como anal骻ico
-	MOVWF ADCON1 				;Se coloca en ADCON1 para que todo se digital 
-	MOVLW 00H 					;Ponemos al puerto B como una salida
-	MOVWF TRISD 				;Se coloca en TRISD
-	BCF STATUS, RP0 			;Retornamos al banco 0
-	MOVLW b'11111001' 			;Frecuencia del reloj
+	CLRF PORTA 			;limpiamos PORTA
+	BSF STATUS, RP0 		;Nos posicionamos en el banco 1 para poder trabajar con TRIS
+	BCF STATUS, RP1 		;Nos cambiamos al banco 1
+	MOVLW 00H 			;Se configura al puerto a y e como anal贸gico
+	MOVWF ADCON1 			;Se coloca en ADCON1 para que todo se digital 
+	MOVLW 00H 			;Ponemos al puerto B como una salida
+	MOVWF TRISD 			;Se coloca en TRISD
+	BCF STATUS, RP0 		;Retornamos al banco 0
+	MOVLW b'11111001' 		;Frecuencia del reloj y configuraci贸n de canal
 	MOVWF ADCON0
-	CLRF PORTD 					;Limpiamos el puertoB
+	CLRF PORTD 			;Limpiamos el puertoD
 
 CONVERTIDOR:
-	;BCF STATUS, C 				;Se limpia a C
-	BSF ADCON0, 2 				;Se inicia la conversi髇 de a/d
-	CALL RETARDO 				;Se llama un retardo
-	BCF ADCON0,2 				;Se finaliza la conversi髇 
-	MOVFW ADRESH 				;Se lee el ediltado de la conversi髇
-	SUBLW D'85' 				;Se resta el primer tercio
-	BTFSC STATUS, C 			;El carry se apaga si es negativo
-	GOTO OP1 					;Si es positivo se va a la op1
-	MOVFW ADRESH 				;Se lee el resultado de la conversi髇 
-	SUBLW D'170' 				;Se resta el segundo tercio
-	BTFSC STATUS, C 			;Si el carry se apaga si el resultado es negativo
-	GOTO OP2 					;Pero si es positivo se va a la op1
-	MOVLW H'07' 				;Si no es ninguna de las anteriores es porque esta en el tercer caso
-	MOVWF PORTD 				;Se pone en el puertob
-	GOTO CONVERTIDOR 			;Se vuelve a INICIO
+	;BCF STATUS, C 			;Se limpia a C
+	BSF ADCON0, 2 			;Se inicia la conversi贸n de a/d
+	CALL RETARDO 			;Se llama un retardo
+	BCF ADCON0,2 			;Se finaliza la conversi贸n 
+	MOVFW ADRESH 			;Se lee el ediltado de la conversi贸n
+	SUBLW D'85' 			;Se resta el primer tercio
+	BTFSC STATUS, C 		;El carry se apaga si es negativo
+	GOTO OP1 			;Si es positivo se va a la op1
+	MOVFW ADRESH 			;Se lee el resultado de la conversi贸n 
+	SUBLW D'170' 			;Se resta el segundo tercio
+	BTFSC STATUS, C 		;Si el carry se apaga si el resultado es negativo
+	GOTO OP2 			;Pero si es positivo se va a la op1
+	MOVLW H'07' 			;Si no es ninguna de las anteriores es porque esta en el tercer caso
+	MOVWF PORTD 			;Se pone un 7 en el puertob
+	GOTO CONVERTIDOR 		;Se vuelve a INICIO
 	
 OP1: 
-	MOVLW H'01' 			;Se pone un 1 en el puerto B
+	MOVLW H'01' 			;Se pone un 1 en el puerto D
 	MOVWF PORTD
 	GOTO CONVERTIDOR
 	
 OP2:
-	MOVLW H'03' 			;Se pone un 3 en el puerto B
+	MOVLW H'03' 			;Se pone un 3 en el puerto D
 	MOVWF PORTD 
 	GOTO CONVERTIDOR 
 
 RETARDO:
-	MOVLW d'25' 				;w= 25 en decimal
-	MOVWF J 					;J = w
+	MOVLW d'25' 			;w= 25 en decimal
+	MOVWF J 			;J = w
 jloop: 
-	MOVWF K 					;K = w
+	MOVWF K 			;K = w
 kloop:
-	DECFSZ K, f 				;Se hace un drecremento en 1 a k
-	goto kloop 					;Regresamos a jloop
-	DECFSZ J, f 				;Se decrementa en 1 a J
-	goto jloop 					;Regresamos a jloop
+	DECFSZ K, f 			;Se hace un drecremento en 1 a k
+	goto kloop 			;Regresamos a jloop
+	DECFSZ J, f 			;Se decrementa en 1 a J
+	goto jloop 			;Regresamos a jloop
 	RETURN
 END
