e37d2a2330ccbcd8ed14169fc181e6f6eb2cde5f
Fernando Maceda <fernando.patricio.maceda@gmail.com>
2022-11-06 16:05:38 -0600

    Update ejer1.asm

--- P6/ejer1.asm
+++ P6/ejer1.asm
@@ -1,43 +1,41 @@
-;Pr醕tica 3 - Ejercicio1
-
-processor 16f877 				;Versi髇 del procesador a utilizar
+processor 16f877 			;Versi贸n del procesador a utilizar
 include <p16f877.inc> 			;Se cargan las librerias y el mapa de memoria
 
-	J EQU 0X20 					;Localidades para la subrutina de retraso 
-	K EQU 0X21 					;Se carga al vector RESET
+	J EQU 0X20 			;Localidades para la subrutina de retraso 
+	K EQU 0X21 			;Se carga al vector RESET
 
-	goto inicio 				;Su ubicaci髇 es en el espacio de memoria "inicio"
-	org 5h 						;Se ubica en el espacio de memoria 5 
+	goto inicio 			;Su ubicaci贸n es en el espacio de memoria "inicio"
+	org 5h 				;Se ubica en el espacio de memoria 5 
 
 inicio:
-	BSF STATUS, RP0 			;Nos posicionamos en el banco 1 para poder trabajar con TRIS
-	BCF STATUS, RP1 			;Nos cambiamos al banco 1
-	MOVLW 00H 					;Se configura al puerto A y E como anal骻ico
-	MOVWF ADCON1 				;Se coloca en ADCON1 para que todo se digital 
-	MOVLW 00H 					;Ponemos al puerto B como una salida
-	MOVWF TRISD 				;Se coloca en TRISB
-	BCF STATUS, RP0 			;Retornamos al banco 0
-	MOVLW b'11111001' 			;Frecuencia del reloj
+	BSF STATUS, RP0 		;Nos posicionamos en el banco 1 para poder trabajar con TRIS
+	BCF STATUS, RP1 		;Nos cambiamos al banco 1
+	MOVLW 00H 			;Se configura al puerto A y E como anal贸gico
+	MOVWF ADCON1 			;Se coloca en ADCON1 para que todo se digital 
+	MOVLW 00H 			;Ponemos al puerto D como una salida
+	MOVWF TRISD 			;Se coloca en TRISD
+	BCF STATUS, RP0 		;Retornamos al banco 0
+	MOVLW b'11111001' 		;Frecuencia del reloj y configuraci贸n de canal
 	MOVWF ADCON0
-	CLRF PORTD 					;Limpiamos el puertoB
+	CLRF PORTD 			;Limpiamos el puertoD
 
 CONVERTIDOR:
-	BSF ADCON0, 2 				;Se inicia la conversi髇 A/D modificando bit2 a 1
-	CALL RETARDO 				;Llamamos a un retardo para que se haga la conversi髇
-	BCF ADCON0, 2 				;Se finaliza la conversi髇 
-	MOVFW ADRESH 				;Se lee el rediltado de la convresi髇 
-	MOVWF PORTD 				;Se carga en el puerto B
-	GOTO CONVERTIDOR 			;Regresamos a Convertidor 
+	BSF ADCON0, 2 			;Se inicia la conversi贸n A/D modificando bit2 a 1
+	CALL RETARDO 			;Llamamos a un retardo para que se haga la conversi贸n
+	BCF ADCON0, 2 			;Se finaliza la conversi贸n 
+	MOVFW ADRESH 			;Se lee el rediltado de la convresi贸n 
+	MOVWF PORTD 			;Se carga en el puerto D
+	GOTO CONVERTIDOR 		;Regresamos a Convertidor 
 
 RETARDO:
-	MOVLW d'25' 				;w= 25 en decimal
-	MOVWF J 					;J = w
+	MOVLW d'25' 			;w= 25 en decimal
+	MOVWF J 			;J = w
 jloop: 
-	MOVWF K 					;K = w
+	MOVWF K 			;K = w
 kloop:
-	DECFSZ K, f 				;Se hace un drecremento en 1 a k
-	goto kloop 					;Regresamos a jloop
-	DECFSZ J, f 				;Se decrementa en 1 a J
-	goto jloop 					;Regresamos a jloop
+	DECFSZ K, f 			;Se hace un drecremento en 1 a k
+	goto kloop 			;Regresamos a jloop
+	DECFSZ J, f 			;Se decrementa en 1 a J
+	goto jloop 			;Regresamos a jloop
 END
 	
